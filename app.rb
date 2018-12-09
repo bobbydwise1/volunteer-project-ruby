e285481da9d346f4a2be1d0a5b7181c1e7cb66b6
Robert Lee <bobbydwise1@hotmail.com>
2018-12-09 09:19:37 -0800

    Add working dynamic submit to index.

--- app.rb
+++ app.rb
@@ -25,13 +25,14 @@ post '/project_add' do
   erb(:index)
 end
 
-get '/project_update' do
+get '/project_management/:id' do
+  id = params[:id]
   @volunteers = Volunteer.all
-  @projects = Project.all()
-  erb(:project_update)
+  @projects = Project.all
+  erb(:project_management)
 end
 
-post '/project_update' do
+post '/project_management/:id' do
   volunteer_name = params.fetch('volunteer_name')
   project_title = params.fetch('title')
   new_volunteer = Volunteer.new({:volunteer_name => volunteer_name, :project_id => project_id, :id => nil})
@@ -43,74 +44,20 @@ post '/project_update' do
   erb(:project_update)
 end
 
-get '/project_update' do
-  @volunteers = Volunteer.all
-  @projects = Project.all()
-  erb(:project_update)
-end
-
-post '/project_update' do
-  volunteer_name = params.fetch('volunteer_name')
-  project_title = params.fetch('title')
-  new_volunteer = Volunteer.new({:volunteer_name => volunteer_name, :project_id => project_id, :id => nil})
-  new_volunteer.save
-  new_project = Project.new({:title => project_title, :id => nil})
-  project_id = new_project.save
-  @volunteers = Volunteer.all
-  @projects = Project.all
-  erb(:project_update)
-end
-
-get '/volunteer_view' do
-  @volunteers = Volunteer.all
-  @projects = Project.all()
-  erb(:volunteer_view)
-end
-
-post '/volunteer_view' do
-  volunteer_name = params.fetch('volunteer_name')
-  project_title = params.fetch('title')
-  new_volunteer = Volunteer.new({:volunteer_name => volunteer_name, :project_id => project_id, :id => nil})
-  new_volunteer.save
-  new_project = Project.new({:title => project_title, :id => nil})
-  project_id = new_project.save
-  @volunteers = Volunteer.all
-  @projects = Project.all
-  erb(:project_update)
-end
-
-get '/volunteer_add' do
-  @volunteers = Volunteer.all
-  @projects = Project.all()
-  erb(:volunteer_add)
-end
-
-post '/volunteer_add' do
-  volunteer_name = params.fetch('volunteer_name')
-  project_title = params.fetch('title')
-  new_volunteer = Volunteer.new({:volunteer_name => volunteer_name, :project_id => project_id, :id => nil})
-  new_volunteer.save
-  new_project = Project.new({:title => project_title, :id => nil})
-  project_id = new_project.save
-  @volunteers = Volunteer.all
-  @projects = Project.all
-  erb(:volunteer_add)
-end
-
-get '/volunteer_assign' do
-  @volunteers = Volunteer.all
-  @projects = Project.all()
-  erb(:volunteer_add)
-end
-
-post '/volunteer_assign' do
-  volunteer_name = params.fetch('volunteer_name')
-  project_title = params.fetch('title')
-  new_volunteer = Volunteer.new({:volunteer_name => volunteer_name, :project_id => project_id, :id => nil})
-  new_volunteer.save
-  new_project = Project.new({:title => project_title, :id => nil})
-  project_id = new_project.save
-  @volunteers = Volunteer.all
-  @projects = Project.all
-  erb(:volunteer_add)
-end
+# get '/project_update' do
+#   @volunteers = Volunteer.all
+#   @projects = Project.all()
+#   erb(:project_update)
+# end
+#
+# post '/project_update' do
+#   volunteer_name = params.fetch('volunteer_name')
+#   project_title = params.fetch('title')
+#   new_volunteer = Volunteer.new({:volunteer_name => volunteer_name, :project_id => project_id, :id => nil})
+#   new_volunteer.save
+#   new_project = Project.new({:title => project_title, :id => nil})
+#   project_id = new_project.save
+#   @volunteers = Volunteer.all
+#   @projects = Project.all
+#   erb(:project_update)
+# end
